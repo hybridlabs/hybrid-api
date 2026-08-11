3b14e5d13a5d9c054d4502b1e76b90186c3bee53
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-08-11 20:31:03 +0300

    Fixed wateranimalfollowcreaturegoal being internal

--- fabric/src/main/java/dev/hybridlabs/hapi/platform/services/FabricPlatformHelper.java
+++ fabric/src/main/java/dev/hybridlabs/hapi/platform/services/FabricPlatformHelper.java
@@ -1,15 +1,6 @@
 package dev.hybridlabs.hapi.platform.services;
 
-import dev.hybridlabs.hapi.CommonClass;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.world.entity.*;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SpawnEggItem;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.nio.file.Path;
-import java.util.function.Supplier;
 
 public class FabricPlatformHelper implements PlatformHelper {
 
@@ -28,18 +19,5 @@ public class FabricPlatformHelper implements PlatformHelper {
     public boolean isDevelopmentEnvironment() {
 
         return FabricLoader.getInstance().isDevelopmentEnvironment();
-    }
-
-    @Override
-    public <T extends Mob> Supplier<SpawnEggItem> registerSpawnEggItem(@NotNull String name,
-                                                                       Supplier<EntityType<T>> entityType,
-                                                                       int backgroundColor, int highlightColor) {
-        return CommonClass.ITEMS.register(name, () -> new SpawnEggItem(entityType.get(), backgroundColor,
-                highlightColor, new Item.Properties()));
-    }
-
-    @Override
-    public Path getConfigDir() {
-        return FabricLoader.getInstance().getConfigDir();
     }
 }
