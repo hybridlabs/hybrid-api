8fd66c5f821f3b49996bc5add3528c2c09cc6a11
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-08-12 10:21:17 +0300

    Something is messed up with trinkets again

---
+++ common/src/main/kotlin/dev/hybridlabs/hapi/client/model/entity/BaseCrustaceanEntityModel.kt
@@ -0,0 +1,36 @@
+package dev.hybridlabs.hapi.client.model.entity
+
+import dev.hybridlabs.hapi.entity.water.base.BaseCrustaceanEntity
+import net.minecraft.resources.ResourceLocation
+import software.bernie.geckolib.model.GeoModel
+
+@Suppress("OVERRIDE_DEPRECATION")
+abstract class BaseCrustaceanEntityModel<T : BaseCrustaceanEntity>(
+    private val namespace: String,
+    private val id: String
+) : GeoModel<T>() {
+
+    override fun getModelResource(animatable: T): ResourceLocation {
+        return ResourceLocation.fromNamespaceAndPath(
+            namespace,
+            "geo/crustacean/$id/$id.geo.json")
+    }
+
+    override fun getTextureResource(animatable: T): ResourceLocation {
+        return ResourceLocation.fromNamespaceAndPath(
+            namespace,
+            "textures/entity/crustacean/$id/$id.png")
+    }
+
+    override fun getAnimationResource(animatable: T): ResourceLocation {
+        return ResourceLocation.fromNamespaceAndPath(
+            namespace,
+            "animations/entity/crustacean/$id/$id.animation.json")
+    }
+
+    fun getLayerTextureResource(layer: String): ResourceLocation {
+        return ResourceLocation.fromNamespaceAndPath(
+            namespace,
+            "textures/entity/crustacean/$id/layers/${id}_$layer.png")
+    }
+}
