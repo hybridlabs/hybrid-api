7b66953b35d38163f282e2214e870ca0cf52c303
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-08-15 01:19:48 +0300

    Added ProgressiveFoodItem

---
+++ common/src/main/kotlin/dev/hybridlabs/hapi/item/ProgressiveFoodItem.kt
@@ -0,0 +1,52 @@
+package dev.hybridlabs.hapi.item
+
+import net.minecraft.advancements.CriteriaTriggers
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.stats.Stats
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.*
+import net.minecraft.world.level.Level
+import kotlin.let
+
+class ProgressiveFoodItem(
+    properties: Properties,
+    private val nextItem: () -> Item?
+) : Item(properties) {
+
+    override fun finishUsingItem(
+        stack: ItemStack,
+        level: Level,
+        entityLiving: LivingEntity
+    ): ItemStack {
+        super.finishUsingItem(stack, level, entityLiving)
+
+        if (entityLiving is ServerPlayer) {
+            CriteriaTriggers.CONSUME_ITEM.trigger(entityLiving, stack)
+            entityLiving.awardStat(Stats.ITEM_USED.get(this))
+        }
+
+        if (entityLiving is Player && !entityLiving.abilities.instabuild) {
+            stack.shrink(1)
+        }
+
+        return nextItem()?.let(::ItemStack) ?: ItemStack.EMPTY
+    }
+
+    override fun getUseDuration(
+        stack: ItemStack,
+        entity: LivingEntity
+    ): Int = 32
+
+    override fun getUseAnimation(stack: ItemStack): UseAnim = UseAnim.EAT
+
+    override fun use(
+        level: Level,
+        player: Player,
+        hand: InteractionHand
+    ): InteractionResultHolder<ItemStack?> {
+        return ItemUtils.startUsingInstantly(level, player, hand)
+    }
+}
