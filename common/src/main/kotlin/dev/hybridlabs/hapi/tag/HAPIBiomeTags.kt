4f8bfaab7cecde49a7f9b260d3863b3e4738f139
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-08-22 17:06:24 +0300

    Fix Rainbow Reef namespace

--- common/src/main/kotlin/dev/hybridlabs/hapi/tag/HAPIBiomeTags.kt
+++ common/src/main/kotlin/dev/hybridlabs/hapi/tag/HAPIBiomeTags.kt
@@ -10,6 +10,7 @@ object HAPIBiomeTags {
     //#region Compatibility Biome Tags
     // rainbow reef
     val RR_WARM_OCEANS = TagKey.create(Registries.BIOME, ResourceLocation("rainbow_reef", "warm_oceans"))
+
     //fintastic
     val MOONY_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation("fintastic", "moony_biomes"))
     val PLECO_BIOMES = TagKey.create(Registries.BIOME, ResourceLocation("fintastic", "pleco_biomes"))
